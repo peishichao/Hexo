89db17d47d2f683860026e271bfac6be09abcda8
963201810@qq.com <963201810@qq.com>
2016-08-13 23:55:50 +0800

    Form auto backup script\'s commit

--- scripts/autoGItSave.js
+++ scripts/autoGItSave.js
@@ -1,10 +1,6 @@
 var exe = require('child_process');
 function run() {
-        console.log("恭喜同步成功");
-        exe.exec('git add --all');
-        exe.exec('git commit -am "Form auto backup script\'s commit"');
-        exe.exec('git push origin master');
-        console.log("恭喜同步成功");
+       exe.execFile('D:/Hexo/GitUpdate.bat');
         }
      hexo.on('deployAfter', function() {
         run();
