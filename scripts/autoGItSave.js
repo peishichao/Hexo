228a687ad578c3497099d5b2a3468d963dd2e587
963201810@qq.com <963201810@qq.com>
2016-08-13 23:59:36 +0800

    Form auto backup script\'s commit

--- scripts/autoGItSave.js
+++ scripts/autoGItSave.js
@@ -1,7 +1,7 @@
 var exe = require('child_process');
 function run() {
-       if(exe.execFile('D:/Hexo/GitUpdate.bat').code == 0){
-        console.log("恭喜你更新GIthub成功");
+       if(exe.execFile('D:/Hexo/GitUpdate.bat').code !== 0){
+        console.log("恭喜你更新Github成功");
        }
         }
      hexo.on('deployAfter', function() {
